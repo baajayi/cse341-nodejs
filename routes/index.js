26a5b4286ee6cf4bbc50216bad93b88fca535ac5
baajayi <delly31@hotmail.com>
2023-01-13 19:34:04 +0100

    avoiding cannot get at home

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,8 @@ const router = express.Router();
 
 router.use('/', require('./swagger'));
 router.use('/contacts', require('./contacts'))
+router.get('/', (req, res) => {
+    res.send('Welcome to the App, Use /contacts to go to the contact in the url page')
+})
 
 module.exports = router;
