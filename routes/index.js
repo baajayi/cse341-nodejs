07ac1a14f9018f28393480effe12fa302a7bb2fb
baajayi <delly31@hotmail.com>
2023-01-18 20:18:54 +0100

    personal project edit welcome msg

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.use('/', require('./swagger'));
 router.use('/plant', require('./plant'))
 router.get('/', (req, res) => {
-    res.send('Welcome to the App, Use /plant to go to the contact in the url page')
+    res.send('Welcome to the App, Use /plant to go to the plant in the url page')
 })
 
 module.exports = router;
