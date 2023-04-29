9b5c3b64eecb9d1690e6fa94aea4e1747724837b
baajayi <delly31@hotmail.com>
2023-04-29 13:54:03 +0100

    just concluded personal project

--- middleware/validate.js
+++ middleware/validate.js
@@ -2,14 +2,14 @@ const validator = require('../helpers/validate.js');
 
 const savePlant = (req, res, next) => {
   const validationRule = {
-        commonName:'required|string',
-        localName: 'required|string',
-        family: 'required|string',
-        genus:'required|string',
-        isMedicinal: 'required|string',
-        flowerColor: 'required|string',
-        isEdible: 'required|string',
-        scientificName:'required|string'
+      commonName:'required|string',
+      localName: 'required|string',
+      family: 'required|string',
+      genus:'required|string',
+      isMedicinal: 'required|string',
+      flowerColor: 'required|string',
+      isEdible: 'required|string',
+      scientificName:'required|string'
   };
   validator(req.body, validationRule, {}, (err, status) => {
     if (!status) {
